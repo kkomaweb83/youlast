9ff5e66f8da95e547126c774fee71b8c53653a2a
kkomaweb83 <kkomaweb@nate.com>
2022-02-11 11:16:05 +0900

    회원 테이블 변경

---
+++ src/main/java/com/kko/youlast/member/entity/MemCom.java
@@ -0,0 +1,38 @@
+package com.kko.youlast.member.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class Member implements MemberCode {
+
+    @Id
+    private String userId;
+
+    private String userName;
+    private String phone;
+    private String password;
+    private LocalDateTime regDt;
+
+    private boolean emailAuthYn;
+    private LocalDateTime emailAuthDt;
+    private String emailAuthKey;
+
+    private String resetPasswordKey;
+    private LocalDateTime resetPasswordLimitDt;
+
+    private boolean adminYn;
+
+    private String userStatus;
+
+}
